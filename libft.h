40ef64aa5071f88e871b2d13ee02e26649083d1b
David Gomez Martin <nakama@MacBook-Air-de-David.local>
2023-11-20 04:25:52 +0100

    añado ft_putstr_fd.c y segundo main

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nakama <nakama@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 04:41:06 by nakama            #+#    #+#             */
-/*   Updated: 2023/11/20 02:47:58 by nakama           ###   ########.fr       */
+/*   Updated: 2023/11/20 04:07:43 by nakama           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,11 @@
 //Desde aqui ya nos evita tener que llamarlas en los .C de las funciones
 # include <string.h>
 
-size_t	ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);//Obtiene longitud
 void	ft_putchar_fd(char c, int fd);//filedescriptor o salida standar 
 int	ft_isalpha(int c);//comprueba letras
+void	ft_putstr_fd(char *s, int fd);//con filedescriptor
+
 
 //Cabeceras de las funciones que queremos incluir en la lib
 
---
+++ secondmain.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+//MAIN actualizado con putstr
+int main()
+{
+	char str[]="hola";
+	char isnumber[]="\nCONTIENE NUMERO";
+	char e= 'e';
+	int n = ft_strlen(str);
+	int i = 0;
+	
+	ft_putstr_fd(str,1);
+	
+	return 0;
+}
