cf49f12206e0f7894bc35bfd758fae5d45b764f2
David Gomez Martin <nakama@MacBook-Air-de-David.local>
2023-12-03 04:59:22 +0100

    norminnete

--- bonus/ft_lstadd_back.c
+++ bonus/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: dgomez-m <aecm.davidgomez@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 01:53:23 by dgomez-m          #+#    #+#             */
-/*   Updated: 2023/12/03 03:48:42 by dgomez-m         ###   ########.fr       */
+/*   Updated: 2023/12/03 04:55:01 by dgomez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list *last;
-	
+	t_list	*last;
+
 	if (!*lst)
 		*lst = new;
 	else
 	{
 		last = ft_lstlast(*lst);
-		last -> next = new;
+		last->next = new;
 	}
 }
